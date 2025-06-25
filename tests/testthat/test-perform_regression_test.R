50b01bd817586b25b68e84a75d0c89bbf1324625
WiegerSchipper <w.r.schipper@tudelft.nl>
2025-06-25 17:07:50 +0200

    write test for `perform_regression_test'

--- tests/testthat/test-perform_regression_test.R
+++ tests/testthat/test-perform_regression_test.R
@@ -1,7 +1,23 @@
 
 test_that("regression test output is well formatted", {
 
-  result = perform_regression_test(rnorm(10), rnorm(10), nBootstrap = 10)
+  n = 100
+
+  # Under H1
+  X_data = rnorm(n)
+  Y_data =  X_data + rnorm(n)   #Y = X + epsilon
+  result = perform_regression_test(X_data, Y_data, nBootstrap = 10)
+
+  expect_true(is.data.frame(result$pvals_df))
+
+  expect_true(all(result$pvals_df$pval >= 0))
+
+  expect_true(all(result$pvals_df$pval <= 1))
+
+  # Under H0
+  X_data = rnorm(n)
+  Y_data =  rep(1, n)  #these values are exactly constant (as b = 0 under H0)
+  result = perform_regression_test(X_data, Y_data, nBootstrap = 10)
 
   expect_true(is.data.frame(result$pvals_df))
 
@@ -9,3 +25,4 @@ test_that("regression test output is well formatted", {
 
   expect_true(all(result$pvals_df$pval <= 1))
 })
+
