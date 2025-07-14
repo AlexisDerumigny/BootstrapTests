28dea93eb0864e730cf3d7e8dd2d22855e5af06f
WiegerSchipper <w.r.schipper@tudelft.nl>
2025-07-14 23:15:48 +0200

    add tests for regression

--- tests/testthat/test-perform_regression_test.R
+++ tests/testthat/test-perform_regression_test.R
@@ -26,3 +26,80 @@ test_that("regression test output is well formatted", {
   expect_true(all(result$pvals_df$pval <= 1))
 })
 
+test_that("Regression test throws errors for wrong input", {
+  n = 10
+
+  # Under H1
+  X = rnorm(n)
+  Y =  X + rnorm(n)   #Y = X + epsilon
+
+  expect_error(perform_regression_test(X, Y, nBootstrap = 10, type_stat = ""))
+  expect_error(perform_regression_test(X, Y, nBootstrap = 10, type_boot = ""))
+  expect_error(perform_regression_test(X, Y, nBootstrap = 10, nBootstrap = ""))
+
+  # Check the `bootstrapOptions`
+  expect_error(perform_regression_test(X, Y, nBootstrap = 10,
+                                       bootstrapOptions = list(norm_type = "K S")))
+  expect_error(perform_regression_test(X, Y, nBootstrap = 10,
+                                       bootstrapOptions = list(typo = "KS")))
+  expect_error(perform_regression_test(X, Y, nBootstrap = 10,
+                                       bootstrapOptions = list(boot_type = "KS")))
+})
+
+
+test_that("Different types of bootstrap options work as expected", {
+
+  n = 10
+
+  # Under H1
+  X = rnorm(n)
+  Y = X + rnorm(n)
+
+  set.seed(10)
+  result_1 = perform_regression_test(X, Y, nBootstrap = 10)
+  set.seed(10)
+  result_2 = perform_regression_test(
+    X, Y, nBootstrap = 10,
+    bootstrapOptions = list(type_boot = "indep",
+                            type_stat = "eq") )
+
+  expect_identical(result_1, result_2)
+
+  set.seed(10)
+  result_3 = perform_regression_test(
+    X, Y, nBootstrap = 10,
+    bootstrapOptions = list(type_boot = "indep") )
+
+  # use the same seed to have equal result
+  expect_identical(result_1, result_3)
+
+  # Give warning for theoreotically invalid bootstrap schemes
+  expect_warning({ perform_regression_test(
+    X, Y, nBootstrap = 10,
+    bootstrapOptions = list(type_boot = "NP",
+                            type_stat = "eq") )
+  })
+
+  # Give warning for theoreotically invalid bootstrap schemes
+  expect_warning({perform_regression_test(
+    X, Y, nBootstrap = 10,
+    bootstrapOptions = list(type_boot = "indep",
+                            type_stat = "cent") )
+  })
+
+  set.seed(10)
+  result_6 = perform_regression_test(X, Y, nBootstrap = 10,
+                                       bootstrapOptions = "all")
+
+  set.seed(10)
+  expect_warning({
+    result_7 = perform_regression_test(X, Y, nBootstrap = 10,
+                                         bootstrapOptions = "all and also invalid")
+  })
+
+  # expect that the results are not identical, as the `result_7' also gives
+  # invalid bootstrap schemes
+  expect_false(identical(result_6, result_7))
+})
+
+
